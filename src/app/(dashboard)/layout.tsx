30ecd280e6b4d16ad5c530c190b71def5deaa81d
Nikita Balashov <n9507246u@gmail.com>
2024-08-21 17:18:30 +0300

    добавлены стили для шаблона для панели управления

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -18,7 +18,9 @@ export default function DashboardLayout({
           </Link>  
           <Menu/>
         </div>  
-        <div className=" w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%]"></div>  
+        <div className=" w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll" >
+            
+        </div>  
       </div>
     </div>
     
