7c82b5f6b8ee9993144c6dd3d5241d197c8afc13
Nikita Balashov <n9507246u@gmail.com>
2024-08-21 21:19:51 +0300

    начало работы с страницей админа

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,7 +1,40 @@
+import UserCard from '@/components/UserCart';
+import { ReactNode } from 'react';
+
 const AdminPage = () => {
     return (
-        <div className="">AdminPage</div>
+        <Wrapper>
+            <Left>
+                <UserCardList/>
+            </Left>
+            <Right>
+                <div>rightSide</div>
+            </Right>
+        </Wrapper>
     )
 }
 
 export default AdminPage;
+
+const Wrapper = ({ children }: { children: ReactNode }) => {
+    return <div className=" p-4 flex gap-4 flex-col md:flex-row ">{children}</div>
+}
+
+const Left = ({ children }: { children: ReactNode }) => {
+    return <div className="w-full lg:w-2/3">
+        {children}
+    </div>
+}
+
+const UserCardList = () => {
+    return <div className="flex gap-4 justify-between flex-wrap">
+        <UserCard type='student'/>
+        <UserCard type='teacher'/>
+        <UserCard type='parent'/>
+        <UserCard type='staff'/>
+    </div>
+}
+
+const Right = ({ children }: { children: ReactNode }) => {
+    return <div className="w-full lg:w-1/3">{children}</div>
+}
