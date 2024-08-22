61113708b8867bde03d8e085ea3ef5a32d858776
Nikita Balashov <n9507246u@gmail.com>
2024-08-22 10:35:06 +0300

    фикс цветов на графике студентов

--- src/components/Charts/Charts.tsx
+++ src/components/Charts/Charts.tsx
@@ -59,12 +59,12 @@ export const CountChart = () => {
       {/* BOTTOM */}
       <div className="flex justify-center gap-16">
         <div className="flex flex-col gap-1">
-          <div className="w-5 h-5 bg-lamaSky rounded-full" />
+          <div className="w-5 h-5 bg-mySky rounded-full" />
           <h1 className="font-bold">1,234</h1>
           <h2 className="text-xs text-gray-300">Boys (55%)</h2>
         </div>
         <div className="flex flex-col gap-1">
-          <div className="w-5 h-5 bg-lamaYellow rounded-full" />
+          <div className="w-5 h-5 bg-myYellow rounded-full" />
           <h1 className="font-bold">1,234</h1>
           <h2 className="text-xs text-gray-300">Girls (45%)</h2>
         </div>
