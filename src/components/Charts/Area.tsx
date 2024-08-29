2d819749a8162b5f68748281728ec8d93fcde1b9
Nikita Balashov <n9507246u@gmail.com>
2024-08-29 12:40:34 +0300

    удалены лишние коментарии

--- src/components/Charts/Area.tsx
+++ src/components/Charts/Area.tsx
@@ -1,22 +1,4 @@
 "use client";
-
 import { ReactNode } from 'react';
-const Area = ({ children }: { children: ReactNode }) => {
-  return <div className="flex flex-row flex-wrap">
-      {children}
-  </div>
-}
-
-
-// const Areaxx = () => {
-//   return <>
-//       <div className="flex flex-row flex-wrap">
-//           <CountChart className="pr-2 mb-2" data={dataCountChart}/>
-//           <AttendanceChart className='mb-2' data={dataAttendanceChart} />
-//           <FinanceChart data={dataFinanceChart}/>
-//       </div>
-      
-//   </>
-// }
-
+const Area = ({ children }: { children: ReactNode }) => <div className="flex flex-row flex-wrap"> {children} </div>
 export default Area;
