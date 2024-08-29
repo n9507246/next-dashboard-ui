7b4680bf9d85dcd2b30278ed4a383adaf39b9e3f
Nikita Balashov <n9507246u@gmail.com>
2024-08-29 13:42:34 +0300

    рефакторинг главной страницы

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,7 +1,7 @@
 
-import { ReactNode } from 'react';
 import { dataAttendanceChart, dataCountChart, dataFinanceChart, events } from '@/lib/fakeData';
 
+import {Wrapper as Content, Wrapper as Main, Wrapper as Asside } from '@/components/Wrapper';
 import Events from '@/components/Events';
 import UserCard from '@/components/UserCard';
 import Charts from '@/components/Charts';
@@ -9,21 +9,21 @@ import Charts from '@/components/Charts';
 
 const AdminPage = () => {
     return (
-        <Content>
-            <Main>
+        <Content className='p-4 flex gap-4 flex-col md:flex-row '>
+            <Main className='w-full lg:w-2/3 flex flex-col gap-8'>
                 <UserCard.List>
                     <UserCard.Item type='student' />
                     <UserCard.Item type='teacher' />
                     <UserCard.Item type='parent' />
                     <UserCard.Item type='staff' />
                 </UserCard.List>
-                <Charts.Area className='flex flex-row flex-wrap'>
+                <Charts.Area className='flex flex-row flex-wrap p-1'>
                     <Charts.Count className='w-full lg:w-1/3 h-[450px] mb-2 pr-2' data={dataCountChart}/>
                     <Charts.Attendance className=' w-full lg:w-2/3 h-[450px] mb-2' data={dataAttendanceChart}/>
                     <Charts.Finance className='w-full h-[500px]' data={dataFinanceChart}/>
                 </Charts.Area> 
             </Main>
-            <Asside>
+            <Asside className='w-full lg:w-1/3 flex flex-col gap-8'>
                 <Events.Area className='bg-white p-4 rounded-md'>
                     <Events.Calendar/> 
                     <Events.List events={events}/>
@@ -35,20 +35,5 @@ const AdminPage = () => {
 
 export default AdminPage;
 
-
-
-const Content = ({ children }: { children: ReactNode }) => {
-    return <div className=" p-4 flex gap-4 flex-col md:flex-row ">{children}</div>
-}
-
-const Main = ({ children }: { children: ReactNode }) => {
-    return <div className="w-full lg:w-2/3 flex flex-col gap-8">
-        {children}
-    </div>
-}
-
-const Asside = ({ children }: { children: ReactNode }) => {
-    return <div className="w-full lg:w-1/3 flex flex-col gap-8">{children}</div>
-}
 
 
---
+++ src/components/Wrapper.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from 'react';
+
+interface WrapperProps {
+    className?: string;
+    children: ReactNode
+}
+
+export const Wrapper: React.FC<WrapperProps> = ({ children, className }) => <div className={className}> {children} </div>
+
+//   Wrapper;
