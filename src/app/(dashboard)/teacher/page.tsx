3deb556dbc6eff1640ed8e336f60e115c4b5435b
Nikita Balashov <n9507246u@gmail.com>
2024-09-11 16:32:27 +0300

    добавлен каркас для станицы преподавателя

--- src/app/(dashboard)/teacher/page.tsx
+++ src/app/(dashboard)/teacher/page.tsx
@@ -1,6 +1,27 @@
+
+import { shedule, events } from '@/lib/fakeData';
+import {Wrapper as Content, Wrapper as Main, Wrapper as Asside } from '@/components/Wrapper';
+import Shedule from '@/components/Shedule';
+import Events from '@/components/Events';
+import Announcements from '@/components/Announcements';
+
+
 const TeacherPage = () => {
     return (
-        <div className="">TeacherPage</div>
+        <Content className='p-4 flex gap-4 flex-col xl:flex-row '>
+        <Main className='w-full xl:w-2/3 flex flex-col gap-8'>
+            <Shedule.Area className='h-full bg-white p-4 rounded-md'>
+                <Shedule.Header>Shedule</Shedule.Header>
+                <Shedule.Calendar events={shedule}/>
+            </Shedule.Area>
+        </Main>
+        <Asside className='w-full xl:w-1/3 flex flex-col gap-8'>
+            <Announcements.Area className='bg-white p-4 rounded-md'>
+                <Announcements.Header/>
+                <Announcements.List/>
+            </Announcements.Area>
+        </Asside>
+    </Content>
     )
 }
 
